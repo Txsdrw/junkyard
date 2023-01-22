91e7e717854b4127bfa47b520e2be16ef1a110b3
Anton <mamchuranton@gmail.com>
2023-01-22 19:01:04 +0600

    destructuring assignment problem

--- 5.7 SetMap.js
+++ 5.7 SetMap.js
@@ -1,29 +1,27 @@
-    //   Фильтрация уникальных элементов массива с помощью SET
+//   Фильтрация уникальных элементов массива с помощью SET
 
-    function unique(arr) {
-        let set = new Set(arr)
-        return Array.from(set)
+function unique(arr) {
+    let set = new Set(arr)
+    return Array.from(set)
+}
+
+let values = ["Hare", "Krishna", "Hare", "Krishna", "Krishna", "Krishna", "Hare", "Hare", ":-O"];
+
+
+//Напишите функцию aclean(arr), которая возвращает массив слов, очищенный от анаграмм.
+
+// отсортированные строки используются как ключи в коллекции Map,
+// для того чтобы сопоставить каждому ключу только одно значение
+
+function aclean(arr) {
+    let map = new Map()
+
+    for (let item of arr) {
+        let sorted = item.toLowerCase().split('').sort().join('')
+        map.set(sorted, item)
     }
+    return Array.from(map.values())
+}
 
-    let values = ["Hare", "Krishna", "Hare", "Krishna",
-        "Krishna", "Krishna", "Hare", "Hare", ":-O"
-    ];
-
-
-    //Напишите функцию aclean(arr), которая возвращает массив слов, очищенный от анаграмм.
-
-    // отсортированные строки используются как ключи в коллекции Map,
-    // для того чтобы сопоставить каждому ключу только одно значение
-
-    function aclean(arr) {
-        let map = new Map()
-
-        for (let item of arr) {
-            let sorted = item.toLowerCase().split('').sort().join('')
-            map.set(sorted, item)
-        }
-        return Array.from(map.values())
-    }
-
-    let annArr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+let annArr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
 
